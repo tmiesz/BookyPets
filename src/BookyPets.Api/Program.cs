edf1e8f78e8c926ad297e83ae356148ca010105b
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-08-25 18:27:39 +0200

    feat(web): hookup CORS and test login

--- src/BookyPets.Api/Program.cs
+++ src/BookyPets.Api/Program.cs
@@ -2,12 +2,22 @@ using BookyPets.Api;
 using BookyPets.Application;
 using BookyPets.Infrastructure;
 
+var BookyPetsWebOrigin = "_bookyPetsWebOrigin";
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration)
-        .AddPresentation();
+        .AddPresentation()
+        .AddCors(opts => opts.AddPolicy(name: BookyPetsWebOrigin,
+                    policy =>
+                    {
+                        policy.WithOrigins("http://localhost:5173")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    })
+                );
 }
 
 var app = builder.Build();
@@ -18,6 +28,7 @@ var app = builder.Build();
     app.UseHttpsRedirection();
     app.UseAuthorization();
     app.MapControllers();
+    app.UseCors(BookyPetsWebOrigin);
 
     app.Run();
 }
