7a44db4f910da71c08f2f7d0fd4666233ba7189f
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-08-30 22:42:24 +0200

    perf(pets): improve getpets

--- src/BookyPets.Infrastructure/Pets/Persistence/PetsRepository.cs
+++ src/BookyPets.Infrastructure/Pets/Persistence/PetsRepository.cs
@@ -1,4 +1,5 @@
 using BookyPets.Application.Common.Interfaces;
+using BookyPets.Domain.BookAggregate;
 using BookyPets.Domain.PetAggregate;
 using BookyPets.Infrastructure.Common.Persistence;
 using Microsoft.EntityFrameworkCore;
@@ -21,21 +22,22 @@ public class PetsRepository(BookyPetsDbContext dbContext) : IPetsRepository
 
     public async Task<List<Pet>> GetPetsAsync(string? search = null)
     {
-        var pets = await _dbContext.Pets.ToListAsync();
-        foreach (var p in pets)
-        {
-            Console.WriteLine($"{p.Name}: FavouriteGenre={p.FavouriteGenre}, ToString={p.FavouriteGenre?.ToString()}");
-        }
+        var pets = _dbContext.Pets.AsQueryable();
 
         if (!string.IsNullOrWhiteSpace(search))
         {
-            var searchTerm = search.Trim().ToLower();
-            pets = [.. pets.Where(pet =>
-                pet.Name.ToLower().Contains(searchTerm) ||
-                (pet.FavouriteGenre != null && pet.FavouriteGenre.Name.ToLower().Contains(searchTerm)))];
+            var searchTerm = search.Trim();
+
+            var matchingGenres = Genre.List
+                .Where(g => g.Name.Contains(searchTerm, StringComparison.InvariantCultureIgnoreCase))
+                .ToList();
+
+            pets = pets.Where(pet =>
+                EF.Functions.Like(pet.Name, $"%{searchTerm}%") ||
+                (pet.FavouriteGenre != null && matchingGenres.Contains(pet.FavouriteGenre)));
         }
 
-        return pets;
+        return await pets.ToListAsync();
     }
 
     public Task UpdatePetAsync(Pet pet)
