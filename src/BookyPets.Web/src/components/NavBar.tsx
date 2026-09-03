732d95b62cf878f92dc3d16b68e797dc90298d30
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-09-03 18:35:52 +0200

    style(web navbar): changes to style

--- src/BookyPets.Web/src/components/NavBar.tsx
+++ src/BookyPets.Web/src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import { AuthContext } from "../AuthContext";
 import { useContext } from "react";
 
 export default function NavBar() {
-    const { user, logout } = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
 
     return <nav className="navbar">
         <div className="navbar-brand">
@@ -12,12 +12,18 @@ export default function NavBar() {
         </div>
 
         <div className="navbar-links">
-            <Link to="/" className="nav-link">Home</Link>
-            {!user.isAuth && <Link to="/auth" className="nav-link">Login</Link>}
-            {user.isAuth && <Link to="/session" className="nav-link">Session</Link>}
-            {user.isAuth && <Link to="/books" className="nav-link">Books</Link>}
-            {user.isAuth && <Link to="/pets" className="nav-link">Pets</Link>}
-            {user.isAuth && <button onClick={logout}>Logout</button>}
+            {user.isAuth && (
+                <>
+                    <Link to="/session">Session</Link>
+                    <Link to="/books">Books</Link>
+                    <Link to="/pets">Pets</Link>
+                </>
+            )}
         </div>
-    </nav>
+
+        <div className="navbar-auth">
+            <Link to="/auth" className="btn-secondary">Login</Link>
+            <Link to="/auth" className="btn-primary">SignUp</Link>
+        </div>
+    </nav >
 }
