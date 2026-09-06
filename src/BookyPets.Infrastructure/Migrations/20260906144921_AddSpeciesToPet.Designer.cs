158f6085ada00c0f8fa853b6597a1f110486d45e
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-09-06 16:55:49 +0200

    feat(pets): ef migrate pets species

--- src/BookyPets.Infrastructure/Migrations/20260906144921_AddSpeciesToPet.Designer.cs
+++ src/BookyPets.Infrastructure/Migrations/20260906144921_AddSpeciesToPet.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BookyPets.Infrastructure.Common.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookyPets.Infrastructure.Migrations
 {
     [DbContext(typeof(BookyPetsDbContext))]
-    partial class BookyPetsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260906144921_AddSpeciesToPet")]
+    partial class AddSpeciesToPet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.3");
---
+++ src/BookyPets.Infrastructure/Migrations/20260906144921_AddSpeciesToPet.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookyPets.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSpeciesToPet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Species",
+                table: "Pets",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Species",
+                table: "Pets");
+        }
+    }
+}
